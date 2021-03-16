36a673e504bf981a65b1f167ff7bd6630e34c420
juulrh <juulrh@gmail.com>
2021-03-16 18:58:54 +0100

    yea

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// lopende digitale KLOK
-// This Day
+
+
+//DATUM
 var today2 = new Date();
 document.getElementById('datum').innerHTML = today2.getDate() + '/' + (today2.getMonth()+1);
 // This Month
-var maanden = new Array('Januari', 'Debruari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December');
+var maanden = new Array('Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December');
 
 document.getElementById('datum').innerHTML = today2.getDate() + ' ' + maanden[today2.getMonth()] + ' ' + today2.getFullYear();
 
 var today = new Date();
 
+//KLOK
 function startTime(){
 //tijd opzoeken
   today = new Date();
@@ -20,7 +22,8 @@ function startTime(){
 
   var t = setTimeout(startTime,500);
 }
-//nul toevoegen als nummer onder de 10 is
+
+//NUL toevoegen als nummer onder de 10 is
 function addLeadingZero(number){
   if(number < 10){
     return '0' + number;
@@ -32,11 +35,12 @@ function addLeadingZero(number){
 
 var dagNacht = today.getHours();
 
-  if (dagNacht > 7 && dagNacht <= 19){
+  if (dagNacht > 6 && dagNacht <= 18){
   //  document.getElementById('afb').src = "zon.png";
     document.getElementsByTagName('body')[0].style.backgroundImage = "url('zon.png')";
+    document.body.style.backgroundPosition = "center";
   } else {
-    document.getElementById('afb').src = "maan.png";
+    document.getElementsByTagName('body')[0].style.backgroundImage = "url('maan.png')";
   }
 
 
