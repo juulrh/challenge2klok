72e4608b1dc0926d8fedc4248584cdfbc1c6070e
juulrh <juulrh@gmail.com>
2021-03-17 03:00:57 +0100

    netjes

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 //DAG
-var d = new Date();
+  var d = new Date();
   var dagweek = new Array("zondag","maandag", "dinsdag","woensdag","donderdag","vrijdag","zaterdag");
   var n = dagweek[d.getDay()];
+
   document.getElementById("dag").innerHTML = n;
 
 //DATUM
-var today2 = new Date();
-document.getElementById('datum').innerHTML = today2.getDate() + '/' + (today2.getMonth()+1);
+  var vandaag = new Date();
 
-var maanden = new Array('Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December');
+  var maanden = new Array('Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December');
 
-document.getElementById('datum').innerHTML = today2.getDate() + ' ' + maanden[today2.getMonth()] + ' ' + today2.getFullYear();
-
-var today = new Date();
+  document.getElementById('datum').innerHTML = vandaag.getDate() + ' ' + maanden[vandaag.getMonth()] + ' ' + vandaag.getFullYear();
 
 //KLOK
-function startTime(){
-//tijd opzoeken
-  today = new Date();
+  var today = new Date();
+  function startTime(){
+  //tijd opzoeken
+    today = new Date();
 
-  var seconds = addLeadingZero(today.getSeconds());
-  var minutes = addLeadingZero(today.getMinutes());
+    var hours = addLeadingZero(today.getHours());
+    var seconds = addLeadingZero(today.getSeconds());
+    var minutes = addLeadingZero(today.getMinutes());
 
-  document.getElementById('klok').innerHTML = today.getHours() + ':' + minutes + ':' + seconds;
+    document.getElementById('klok').innerHTML = hours + ':' + minutes + ':' + seconds;
 
-  var t = setTimeout(startTime,500);
+    var t = setTimeout(startTime,500);
 }
 
 //NUL toevoegen als nummer onder de 10 is
-function addLeadingZero(number){
-  if(number < 10){
-    return '0' + number;
+  function addLeadingZero(number){
+    if(number < 10){
+      return '0' + number;
+    }
+      return number;
   }
-    return number;
-}
 
-// AFB maan/zon veranderen
+//AFB maan/zon veranderen & Background kleur & tekst dag/nacht
+  var dagNacht = today.getHours();
 
-var dagNacht = today.getHours();
+    if (dagNacht > 6 && dagNacht <= 18){
+      document.getElementsByTagName('body')[0].style.backgroundImage = "url('zon.png')";
+      document.body.style.backgroundColor = 'lightblue';
+      document.getElementById("dn").innerHTML= "DAG";
 
-  if (dagNacht > 6 && dagNacht <= 18){
-    document.getElementsByTagName('body')[0].style.backgroundImage = "url('zon.png')";
-    document.body.style.backgroundColor = 'lightblue';
-    document.getElementById("dn").innerHTML= "DAG";
+    } else {
+      document.getElementsByTagName('body')[0].style.backgroundImage = "url('maan2.png')";
+      document.body.style.backgroundColor = 'black';
+      document.getElementById("dn").innerHTML= "NACHT";
 
-  } else {
-    document.getElementsByTagName('body')[0].style.backgroundImage = "url('maan2.png')";
-    document.body.style.backgroundColor = 'black';
-    document.getElementById("dn").innerHTML= "NACHT";
-
-  }
+    }
